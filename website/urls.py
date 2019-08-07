f11deddac7177f73ee51d1392b46f365b63ce4d1
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-08-07 01:33:14 -0400

    Added homepage UI

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from . import views
+from .views import *
 
 # Application Routes (URLs)
 
@@ -9,9 +9,5 @@ app_name = 'website'
 urlpatterns = [
 
 	# Home Page
-	url(r'^$', views.home_page, name='home_page'),
-
-	# Profile Page
-	url(r'^profile$', views.profile, name='profile'),
-
+	url(r'^$', HomePageView.as_view(), name='home_page'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 # Application Views
 
-# Home
-def home_page(request):
-	return render(request, 'website/home_page.html')
 
-# Profile
-@login_required
-def profile(request):
-	return render(request, 'website/profile.html')
+# Home
+class HomePageView(TemplateView):
+    template_name = 'website/home_page.html'
